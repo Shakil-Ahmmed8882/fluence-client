cf0638fbb177844b5ac76094ab0cbd377e8e36d1
shakil8882 <shakilahmmed8882@gmail.com>
2025-01-29 12:42:20 +0600

    Refractor:home page

--- src/app/(home)/components/HeroHeader.tsx
+++ src/app/(home)/components/HeroHeader.tsx
@@ -10,7 +10,7 @@ const HeroHeader = () => {
         width={100}
         height={100}
       />
-      <h1 className="text-5xl font-bold -ml-3 gradient-text">LUENCE</h1>
+      <h1 className="text-5xl font-bold -ml-3 gradient-text tracking-[4px]">LUENCE</h1>
       </div>
       <p className="text-xl text-[#c9c9c9] mb-12 text-center max-w-2xl">
         Master English like never before. Your journey to fluency starts here.
